b11720b01a158b322d03c00312447c4613eef65b
BC_IML <lelj28819@gmail.com>
2022-06-10 08:59:36 -0400

    Adding Icons for main Title card

--- assets/js/weatherapp.js
+++ assets/js/weatherapp.js
@@ -5,6 +5,12 @@ var ForecastW = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&
 var cordW = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=57&lon=-2.15&appid=85b081d82a8b2923ac904659cddb3896"
 //var cordW = "https://api.openweathermap.org/data/2.5/forecast/daily?&appid=85b081d82a8b2923ac904659cddb3896"
 
+var Title = $('#Title')
+var TempActual = $('#TempActual')
+var TempMin = $('#TempMin')
+var TempMax = $('#TempMax')
+var iconActual = $('#iconActual')
+var Wdescription = $('#Wdescription')
 
 
 
@@ -21,12 +27,18 @@ var cordW = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=57&lon=-
             console.log(value)
             var lat = value['latitude']
             var long = value['longitude']
+            Title.text(value['title'])
+           
             $.ajax({
-                url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=85b081d82a8b2923ac904659cddb3896`
+                url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=85b081d82a8b2923ac904659cddb3896&units=imperial`
             }).then(function(data) {
-               //$('.greeting-id').append(data.id);
-               //$('.greeting-content').append(data.content);
                console.log(data);
+               TempActual.text(`Actual Temp: ${data['main']['temp']} F`)
+               TempMin.text(`Min Temp: ${data['main']['temp_min']} F`)
+               TempMax.text(`Max Temp: ${data['main']['temp_max']} F`)
+               console.log(data['weather'][0]['icon'])
+               Wdescription.text(data['weather'][0]['description'])
+               iconActual.attr("src", `http://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png`)
             });
 
         }
