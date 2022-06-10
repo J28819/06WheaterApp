d4bddbf4e4a9c68b086ebcaf2fe36a30ca71cbe6
BC_IML <lelj28819@gmail.com>
2022-06-09 23:40:06 -0400

    Adding geolocation for autofill the City and get the weather from that place

--- assets/js/weatherapp.js
+++ assets/js/weatherapp.js
@@ -3,12 +3,33 @@ var city = "London, uk";
 var ActualW = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 var ForecastW = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
 var cordW = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=57&lon=-2.15&appid=85b081d82a8b2923ac904659cddb3896"
-var cordW = "https://api.openweathermap.org/data/2.5/forecast/daily?&appid=85b081d82a8b2923ac904659cddb3896"
+//var cordW = "https://api.openweathermap.org/data/2.5/forecast/daily?&appid=85b081d82a8b2923ac904659cddb3896"
 
-    $.ajax({
-        url: ForecastW
-    }).then(function(data) {
-       //$('.greeting-id').append(data.id);
-       //$('.greeting-content').append(data.content);
-       console.log(data);
+
+
+
+
+    var $results = document.querySelector('.results');
+    //var appendToResult = $results.insertAdjacentHTML.bind($results, 'afterend');
+    var appendToResult
+
+//Teleport API Autocomplete
+    TeleportAutocomplete.init('.my-input').on('change', function(value) {
+        //appendToResult = JSON.stringify(value, null, 2)
+        appendToResult = value
+        if (value !== null){
+            console.log(value)
+            var lat = value['latitude']
+            var long = value['longitude']
+            $.ajax({
+                url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=85b081d82a8b2923ac904659cddb3896`
+            }).then(function(data) {
+               //$('.greeting-id').append(data.id);
+               //$('.greeting-content').append(data.content);
+               console.log(data);
+            });
+
+        }
+        
+      
     });
